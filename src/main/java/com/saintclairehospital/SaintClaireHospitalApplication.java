dfdb28229b5a119c6525b636706daa2889a37000
justinok <38015681+justinok@users.noreply.github.com>
2022-07-28 14:51:55 -0500

    Creation of the structure of the project
    Starting the process to conect SQL

---
+++ src/main/java/com/saintclairehospital/SaintClaireHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.saintclairehospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaintClaireHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaintClaireHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saintclairehospital/SaintClaireHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saintclairehospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintClaireHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
